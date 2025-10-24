c447a84118666d6d1b36a64b078b153b8e33bbab
Ujjwal Rai <urai31382@gmail.com>
2025-10-25 01:10:20 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -17,6 +17,9 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 import App from './App.jsx'
+import 'leaflet/dist/leaflet.css';
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
